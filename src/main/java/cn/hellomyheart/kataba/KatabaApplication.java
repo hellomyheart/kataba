0982b801fed58379c6a9f8e8c58708f38f386f86
stephen shen <723809534@qq.com>
2020-10-19 15:35:32 +0800

    init

---
+++ src/main/java/cn/hellomyheart/kataba/KatabaApplication.java
@@ -0,0 +1,13 @@
+package cn.hellomyheart.kataba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatabaApplication.class, args);
+    }
+
+}
